4a983229ab9c595cc171b5d576c8b370e32bfd77
Jesús Sánchez-Oro <jesus.sanchezoro@gmail.com>
2021-01-14 09:31:29 +0100

    Complete code for AVLTree and RBTree

--- material/tree/binarysearchtree/AVLTree.java
+++ material/tree/binarysearchtree/AVLTree.java
@@ -1,11 +1,11 @@
 package material.tree.binarysearchtree;
 
-import material.Position;
-
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
+
+import material.Position;
 
 
 /**
@@ -14,7 +14,7 @@ import java.util.List;
  * @author A. Duarte, J. Vélez
  */
 public class AVLTree<E> implements BinarySearchTree<E> {
-	
+
 	//We need this class to store the height of each BTNode
     private class AVLInfo<T> implements Comparable<AVLInfo<T>>, Position<T>{
 
@@ -27,11 +27,11 @@ public class AVLTree<E> implements BinarySearchTree<E> {
             this.pos = null;
             this.height = 1;
         }
-        
+
         public void setTreePosition(Position<AVLInfo<T>> pos){
         	this.pos = pos;
         }
-        
+
         public Position<AVLInfo<T>> getTreePosition(){
         	return this.pos;
         }
@@ -54,7 +54,7 @@ public class AVLTree<E> implements BinarySearchTree<E> {
             if (element instanceof Comparable && o.element instanceof Comparable) {
                 Comparable <T> c1 = (Comparable<T>) element;
                 return c1.compareTo(o.element);
-                
+
             } else {
                 throw new ClassCastException("Element is not comparable");
             }
@@ -65,7 +65,7 @@ public class AVLTree<E> implements BinarySearchTree<E> {
             return this.getElement().toString();
         }
     }
-    
+
 
     private class AVLTreeIterator<T> implements Iterator<Position<T>> {
 
@@ -91,7 +91,7 @@ public class AVLTree<E> implements BinarySearchTree<E> {
             it.remove();
         }
     }
-    
+
     private LinkedBinarySearchTree<AVLInfo<E>> bst;
     private ReestructurableBinaryTree<AVLInfo<E>> resBT;
 
@@ -106,40 +106,61 @@ public class AVLTree<E> implements BinarySearchTree<E> {
      * @param c the comparator used to sort the nodes in the tree
      */
     public AVLTree(Comparator<E> c) {
-        throw new RuntimeException("Not implemented.");
+        Comparator<AVLInfo<E>> avlComparator = (o1,o2)->c.compare(o1.getElement(), o2.getElement());
+        bst = new LinkedBinarySearchTree<>(avlComparator);
+        resBT = new ReestructurableBinaryTree<>();
+        bst.binTree = resBT;
     }
 
 
 
     @Override
     public Position<E> find(E value) {
-        throw new RuntimeException("Not implemented.");
+        AVLInfo<E> searchedValue = new AVLInfo<>(value);
+        Position<AVLInfo<E>> output = bst.find(searchedValue);
+
+        if (output == null) {
+            return null;
+        }
+        return output.getElement();
     }
 
     @Override
     public Iterable<Position<E>> findAll(E value) {
-        throw new RuntimeException("Not implemented.");
+        AVLInfo<E> searchedValue = new AVLInfo<>(value);
+    	List<Position<E>> aux = new ArrayList<> ();
+        for (Position<AVLInfo<E>> n : bst.findAll(searchedValue)){
+			aux.add(n.getElement());
+        }
+        return aux;
     }
 
     @Override
     public boolean isEmpty() {
-        throw new RuntimeException("Not implemented.");
+        return bst.isEmpty();
     }
 
-   
+
     @Override
     public int size() {
-        throw new RuntimeException("Not implemented.");
+        return bst.size();
     }
 
-  
+
 
     /**
      * Returns whether a node has balance factor between -1 and 1.
      */
     private boolean isBalanced(Position<AVLInfo<E>> p) {
 
-        throw new RuntimeException("Not implemented.");
+        AVLInfo<E> right = bst.binTree.right(p).getElement();
+        AVLInfo<E> left = bst.binTree.left(p).getElement();
+
+        final int leftHeight = (left == null) ? 0 : left.getHeight();
+        final int rightHeight = (right == null) ? 0 : right.getHeight();
+
+        final int bf = leftHeight - rightHeight;
+        return ((-1 <= bf) && (bf <= 1));
     }
 
     /**
@@ -147,11 +168,38 @@ public class AVLTree<E> implements BinarySearchTree<E> {
      */
     private Position<AVLInfo<E>> tallerChild(Position<AVLInfo<E>> p) {
 
-        throw new RuntimeException("Not implemented.");
+        Position<AVLInfo<E>> right = bst.binTree.right(p);
+        Position<AVLInfo<E>> left = bst.binTree.left(p);
+
+        final int leftHeight = (left.getElement() == null) ? 0 : left.getElement().getHeight();
+        final int rightHeight = (right.getElement() == null) ? 0 : right.getElement().getHeight();
+
+        if (leftHeight > rightHeight) {
+            return left;
+        } else if (leftHeight < rightHeight) {
+            return right;
+        }
+
+        // equal height children - break tie using parent's type
+        if (bst.binTree.isRoot(p)) {
+            return left;
+        }
+
+        if (p == bst.binTree.left(bst.binTree.parent(p))) {
+            return left;
+        } else {
+            return right;
+        }
     }
 
     private void calculateHeight(Position<AVLInfo<E>> p) {
-        throw new RuntimeException("Not implemented.");
+        Position<AVLInfo<E>> left = bst.binTree.left(p);
+        Position<AVLInfo<E>> right = bst.binTree.right(p);
+
+        final int leftHeight = (left.getElement() == null) ? 0 : left.getElement().getHeight();
+        final int rightHeight = (right.getElement() == null) ? 0 : right.getElement().getHeight();
+
+        p.getElement().setHeight(1 + Math.max(leftHeight, rightHeight));
     }
 
     /**
@@ -160,7 +208,22 @@ public class AVLTree<E> implements BinarySearchTree<E> {
      * perform a trinode restructuring if it's unbalanced.
      */
     private void rebalance(Position<AVLInfo<E>> zPos) {
-        throw new RuntimeException("Not implemented.");
+        if (bst.binTree.isInternal(zPos)) {
+            calculateHeight(zPos);
+        }
+        while (!bst.binTree.isRoot(zPos)) { // traverse up the tree towards the
+            // root
+            zPos = bst.binTree.parent(zPos);
+            calculateHeight(zPos);
+            if (!isBalanced(zPos)) {
+                // perform a trinode restructuring at zPos's tallest grandchild
+                Position<AVLInfo<E>> xPos = tallerChild(tallerChild(zPos));
+                zPos = this.resBT.restructure(xPos, bst);
+                calculateHeight(bst.binTree.left(zPos));
+                calculateHeight(bst.binTree.right(zPos));
+                calculateHeight(zPos);
+            }
+        }
     }
 
     /**
@@ -168,7 +231,11 @@ public class AVLTree<E> implements BinarySearchTree<E> {
      */
     @Override
     public Position<E> insert(E e) {
-        throw new RuntimeException("Not implemented.");
+        AVLInfo<E> aux = new AVLInfo<>(e);
+        Position<AVLInfo<E>> internalNode = bst.insert(aux);
+        aux.setTreePosition(internalNode);
+        rebalance(internalNode);
+        return aux;
     }
 
     /**
@@ -176,19 +243,36 @@ public class AVLTree<E> implements BinarySearchTree<E> {
      */
     @Override
     public E remove(Position<E> pos) throws RuntimeException {
-        throw new RuntimeException("Not implemented.");
+        AVLInfo<E> aux = checkPosition(pos);
+
+        E toReturn = pos.getElement(); // entry to be returned
+        Position<AVLInfo<E>> remPos = bst.getLeafToRemove(aux.getTreePosition());
+
+        Position<AVLInfo<E>> actionPos = bst.binTree.sibling(remPos);
+        bst.removeLeaf(remPos);
+        rebalance(actionPos); // rebalance up the tree
+        return toReturn;
     }
 
     @Override
 	public Iterator<Position<E>> iterator() {
-        throw new RuntimeException("Not implemented.");
+		Iterator<Position<AVLInfo<E>>> bstIt = bst.iterator();
+		AVLTreeIterator <E> it = new AVLTreeIterator<E>(bstIt);
+		return it;
 	}
-    
+
     /**
      * If v is a good tree node, cast to TreePosition, else throw exception
      */
     private AVLInfo<E> checkPosition(Position<E> p) throws RuntimeException {
-        throw new RuntimeException("Not implemented.");
+        if (p == null) {
+            throw new RuntimeException("The position of the AVL node is null");
+        } else if (!(p instanceof AVLInfo)) {
+            throw new RuntimeException("The position of the AVL node is not AVL");
+        } else {
+        	AVLInfo<E> aux = (AVLInfo<E>) p;
+            return aux;
+        }
     }
 
     /**
@@ -196,24 +280,23 @@ public class AVLTree<E> implements BinarySearchTree<E> {
      */
 
     public Iterable<Position<E>> findRange(E minValue, E maxValue) throws RuntimeException{
-        throw new RuntimeException("Not implemented.");
+        throws new RuntimeException("Not yet implemented");
     }
 
     /**
      * Ejercicio 2: first, last, successors, predecessors
      */
     public Position<E> first() throws RuntimeException {
-        throw new RuntimeException("Not implemented.");
+        throws new RuntimeException("Not yet implemented");
     }
     public Position<E> last() throws RuntimeException {
-        throw new RuntimeException("Not implemented.");
+        throws new RuntimeException("Not yet implemented");
     }
     public Iterable<Position<E>> successors(Position<E> pos){
-        throw new RuntimeException("Not implemented.");
+        throws new RuntimeException("Not yet implemented");
 
     }
     public Iterable<Position<E>> predecessors(Position<E> pos){
-        throw new RuntimeException("Not implemented.");
+        throws new RuntimeException("Not yet implemented");
     }
 }
-
